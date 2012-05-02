cb30ab3cbdd77f03d9eed04a6d58440c44824cc2
Marc Paradise <marc@opscode.com>
2012-05-02 13:27:04 -0700

    FileAccessControl now expects two resources, not resource + path

--- chef/lib/chef/provider/cookbook_file.rb
+++ chef/lib/chef/provider/cookbook_file.rb
@@ -77,7 +77,12 @@ class Chef
 
       # Set file permissions to match expectation 
       def set_file_access_controls(path)
-        ac = Chef::FileAccessControl.new(@new_resource, path)
+        temp_res = Chef::Resource::CookbookFile.new(@new_resource.name)
+        temp_res.group(new_resource.group)
+        temp_res.owner(new_resource.owner)
+        temp_res.mode(new_resource.mode)
+        temp_res.path(path)
+        ac = Chef::FileAccessControl.new(@new_resource, temp_res)
         ac.set_all
       end
 
